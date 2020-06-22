39735bb2fd4b92fc9f74ea4c11861ecd3336e4a3
Damian <damiianalmada06@gmail.com>
2020-06-22 18:51:08 -0400

    nueva combinacion

--- enviar.php
+++ enviar.php
@@ -42,7 +42,7 @@ try {
     $mail->Body    = $contenido;
 
     $mail->send();
-    echo 'El mensaje se envio correctamente';
+    header("Location: https://damianar.herokuapp.com/")
 } catch (Exception $e) {
     echo "Error al enviar el mensaje: {$mail->ErrorInfo}";
 }
