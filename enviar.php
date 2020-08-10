b2523047baf60e29b4e4918534f59e5874024871
Damian <damiianalmada06@gmail.com>
2020-08-10 18:51:12 -0400

    commit php

--- enviar.php
+++ enviar.php
@@ -23,7 +23,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'webdamianal@gmail.com';                     // SMTP username
-    $mail->Password   = 'portafolio44'; //desde que cuente se enviara todo                          // SMTP password
+    $mail->Password   = 'asaditoymandioca'; //desde que cuente se enviara todo                          // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
