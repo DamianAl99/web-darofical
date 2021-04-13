eef7308a8c1334ff4e27d374f910b83dbae4c7cf
Damian <damiianalmada06@gmail.com>
2021-04-13 11:43:57 -0400

    Mejorado de Proyectos

--- js/main.js
+++ js/main.js
@@ -23,3 +23,54 @@ contact.addEventListener("click", function(){
     menu.classList.toggle("show");
 });
 
+const ListaDeProyectos = [
+    {
+        title: 'MINDFULNESS',
+        description: 'Web Informativa. Contiene informacion sobre la meditacin, practicas, Centros de meditacion',
+        url: 'https://meditacion.vercel.app'
+    },
+    {
+        title: 'COMIDAS PARAGUAYAS',
+        description: 'Web Informativa. Contiene informacion sobre comidas, recetas, tutoriales',
+        url: 'https://food-rho.vercel.app/home'
+    },
+    {
+        title: 'WEB DE NOTAS',
+        description: 'Web para guardar Notas',
+        url: 'https://damianal99.github.io/Nota/'
+    },
+    {
+        title: 'COMPLOT',
+        description: 'Web similar a la de Complot Paraguay',
+        url: 'https://damianal99.github.io/complot/'
+    },
+    {
+        title: 'API PAISES',
+        description: 'Web Informativa. Buscador de Paises con Numero de Habitantes y mas.',
+        url: 'https://searchpais.herokuapp.com/index.html'
+    }
+];
+
+const mainProyects = document.querySelector(".content-proyects");
+let forProyect = '';
+
+
+
+for(let i = 0; i < ListaDeProyectos.length; i++){
+    forProyect += `
+    <a href="${ListaDeProyectos[i].url}" class="all">
+        <p>${ListaDeProyectos[i].title}</p>
+        <div class="proyec">
+        ${ListaDeProyectos[i].description}
+        </div>
+    </a>
+    `;
+}
+//console.log(forProyect);
+mainProyects.innerHTML = `
+            <div class="title">
+                 <h3>PROYECTS</h3>
+             </div>
+            ${forProyect}
+`;
+
